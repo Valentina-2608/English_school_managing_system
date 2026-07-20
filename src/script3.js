4fe0587ee3253d327909f3d5504262b86a16df87
Valetina-2608 <valusha2608@gmail.com>
2026-07-21 00:07:37 +0300

    Add script3.js

---
+++ src/script3.js
@@ -0,0 +1,57 @@
+/* Add students to Firestore Database */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getFirestore, addDoc, collection} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+   const firebaseConfig = {
+    apiKey: "AIzaSyCHbc9r1xeUYt0Mz09vk3Tvf-1Zp9J_vNk",
+    authDomain: "english-school-managing-system.firebaseapp.com",
+    projectId: "english-school-managing-system",
+    storageBucket: "english-school-managing-system.firebasestorage.app",
+    messagingSenderId: "292022942256",
+    appId: "1:292022942256:web:c129c711f179e74c098bf5"
+};
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const dbRef = collection(db, 'Students');
+
+
+let form_students = document.getElementById('form_students');
+
+form_students.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var studentFirstName = document.getElementById('student_first_name').value;
+  var studentLastName = document.getElementById('student_last_name').value;
+  var studentPhone = document.getElementById('student_phone').value;
+  var studentEmail = document.getElementById('student_email').value;
+  var studentGroup = document.getElementById('student_group').value;
+  const dbRef = collection(db, 'Students')
+    const data = {
+        student_first_name: studentFirstName,
+        student_last_name: studentLastName,
+        student_phone: studentPhone,
+        student_email: studentEmail,
+        student_group: studentGroup
+      };
+     addDoc(dbRef, data)
+    .then(()=>{
+      alert('Student was added successfully')
+    })
+    .catch((error)=>{
+      alert('Error');
+      console.error(error);
+    });
+
+
+
+
+
+
+})
